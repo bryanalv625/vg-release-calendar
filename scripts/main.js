bf08a083826a1cf9af087fd462957edffd61c286
bryanalv625 <b.alvarado625@gmail.com>
2025-01-07 21:15:04 -0600

    addeddropdown

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,20 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function dropFunction() {
+    document.getElementById("dropDown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
